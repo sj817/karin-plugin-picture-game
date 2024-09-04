d03fb82ca5db3dc76d84bb9d5d154f564e37ff36
Nepenthe <Ttjonne@outlook.com>
2024-09-04 14:56:59 +0800

    更改资源位置，移除无用文件

--- src/apps/example.ts
+++
@@ -1,6 +0,0 @@
-// import { karin } from 'node-karin'
-
-// export const hello = karin.command(/^#你好$/, async (e) => {
-//   await e.reply('hello', { at: false, recallMsg: 0, reply: true })
-//   return true
-// })
